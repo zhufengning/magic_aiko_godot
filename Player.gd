3ebb9e666fb9d3ec7344d2ab8ab7f073d2b67b95
zhufenging <z@zhufn.fun>
2022-02-18 01:29:58 +0800

    hair

--- Player.gd
+++ Player.gd
@@ -6,10 +6,12 @@ const gra = 800
 var speedy = 0
 func _ready():
 	print("hello")
+	print(Vector2(-1, 0).angle())
 	screen_size = get_viewport_rect().size
 	
 func  _physics_process(delta):
 	speedy += delta * gra
+
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		$AnimatedSprite.flip_h = true
@@ -19,13 +21,10 @@ func  _physics_process(delta):
 		$AnimatedSprite.flip_h = false
 		velocity.x -= 1
 		$Particles2D.position = Vector2(9, 40)
-	# if Input.is_action_pressed("move_down"):
-	# 	velocity.y += 1
-	# 	$AnimatedSprite.play("walk")
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 		speedy = 0
-	# print(velocity)
+	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		if $AnimatedSprite.animation == "stop":
@@ -35,35 +34,10 @@ func  _physics_process(delta):
 		if $AnimatedSprite.animation == "walk":
 			$AnimatedSprite.play("stop")
 		$Particles2D.emitting = false
+
 		
-#		$AnimatedSprite.play()
-#	else:
-#		$AnimatedSprite.stop()
 	velocity += Vector2(0, speedy)
-
-
-	velocity = move_and_slide(velocity)
-	# for i in get_slide_count():
-	# 	var collision = get_slide_collision(i)
-	# 	print("I collided with ", collision.collider.name)
-	
-	# position += velocity * delta
-	# position.x = clamp(position.x, 0, screen_size.x)
-	# position.y = clamp(position.y, 0, screen_size.y)
-
-#	if velocity.x != 0:
-#		$AnimatedSprite.animation = "walk"
-#		$AnimatedSprite.flip_v = false
-#		# See the note below about boolean assignment.
-#		$AnimatedSprite.flip_h = velocity.x < 0
-#	elif velocity.y != 0:
-#		$AnimatedSprite.animation = "up"
-#		$AnimatedSprite.flip_v = velocity.y > 0
-#
-#	if velocity.x < 0:
-#		$AnimatedSprite.flip_h = true
-#	else:
-#		$AnimatedSprite.flip_h = false
+	move_and_slide(velocity)
 		
 func start(pos):
 	speedy = 0
